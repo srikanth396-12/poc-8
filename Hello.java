85001bee984cb6e79859e3cddde8e2c320a6b877
srikanth396-12 <saisrikanthre.tetal@hcltech.com>
2026-04-01 09:41:20 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,25 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+ 
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("WebHook Works test");
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+ 
+        server.createContext("/", new HttpHandler() {
+            public void handle(HttpExchange exchange) throws IOException {
+                String response = "Hello from Jenkins Docker Sonar POC 🚀";
+                exchange.sendResponseHeaders(200, response.length());
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        });
+ 
+        server.start();
+        System.out.println("Server started on port 8080");
     }
 }
